b5ff7415c99c3764d7ff58331db7eb10248f270b
Nadin12 <pohlebina.odessa@gmail.com>
2024-10-12 11:05:51 +0300

    Create connecting file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Connecting the configuration file
+require_once 'config.php';
+
+// Connecting files with functions
+require_once 'actions.php';
+require_once 'helpers.php';
+require_once 'validators.php';
+
+// Calling the initialization function
+init();
