2e2d570e4f32792dd0a4a1ddfb93c054854f701e
Vladyslav <mvi110601@gmail.com>
2024-10-13 23:07:21 +0300

    validators,header,form_page

---
+++ inc/validators.php
@@ -0,0 +1,26 @@
+<?php
+$email = [];
+function validateEmail($email) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+$phone = [];
+function validatePhone($phone) {
+    return preg_match('/^\+?[0-9]\d{1,14}$/', $phone);
+}
+$name = [];
+function validateName($name) {
+    if (empty($name)) {
+        return "The name cannot be empty.";
+    }
+
+    $length = strlen($name);
+    if ($length < 3 || $length > 20) {
+        return "The name should be kept from 3 to 20 characters.";
+    }
+
+    return null;
+}
