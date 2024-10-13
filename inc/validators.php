229286f4952fd59ac89749996a13ba741d06d860
Vladyslav <mvi110601@gmail.com>
2024-10-13 23:15:49 +0300

    update

--- inc/validators.php
+++ inc/validators.php
@@ -1,5 +1,4 @@
 <?php
-$email = [];
 function validateEmail($email) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return true;
@@ -7,11 +6,9 @@ function validateEmail($email) {
         return false;
     }
 }
-$phone = [];
 function validatePhone($phone) {
     return preg_match('/^\+?[0-9]\d{1,14}$/', $phone);
 }
-$name = [];
 function validateName($name) {
     if (empty($name)) {
         return "The name cannot be empty.";
