1a5f02de47bcd6866a346bd96613c850aa1cd7bd
Viktoriia Danylenko <danylenko.viktoriia5@gmail.com>
2024-10-14 14:27:46 +0300

    Correct including of the files in default_template.php

--- views/templates/default_template.php
+++ views/templates/default_template.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
-    <title>Мой сайт</title>
+    <title><?php echo siteName; ?></title>
 </head>
+
 <body>
-<header>
-    <?php include 'header.php';?>
-</header>
-<nav>
-    <?php include 'menu.php';?>
-</nav>
-<main>
-    <?php
-    // Getting the page value from the GET parameter
-    $pages = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+    <header>
+        <?php include __DIR__ . '/../common/header.php'; ?>
+    </header>
+    <nav>
+        <?php include __DIR__ . '/../common/menu.php'; ?>
+    </nav>
+    <main>
+        <?php
+        // Getting the action value from the GET parameter
+        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 
-    // Forming the path to the page file
-    $pageFile = 'pages/' . $pages . '.php';
+        // Forming the path to the page file
+        $pageFile = __DIR__ . '/../pages/' . $action . '_page.php';
 
-    // Checking if the file exists
-    if ($pages && file_exists($pageFile)) {
-        include $pageFile; // Include the page file if it exists
-    } else {
-        // If the file is not found, we display a 404 message
-        echo '<h1>404 - Страница не найдена</h1>';
-    }
-    ?>
-</main>
-<footer>
-    <?php include 'footer.php';?>
-</footer>
+        // Checking if the file exists
+        if ($action && file_exists($pageFile)) {
+            include $pageFile; // Include the page file if it exists
+        } else {
+            // If the file is not found, we display a 404 message
+            echo '<h1>404 - Страница не найдена</h1>';
+        }
+        ?>
+    </main>
+    <footer>
+        <?php include __DIR__ . '/../common/footer.php'; ?>
+    </footer>
 </body>
+
 </html>
