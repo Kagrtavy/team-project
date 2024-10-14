d71d186fd2a08591622c5c715c1690515cac015e
rynajs <145012957+rynajs@users.noreply.github.com>
2024-10-14 11:48:21 +0300

    Delete inc directory

--- inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('siteName', 'PHP Team Project');
-define('footerText', 'PHP Team Project, October 2024. All rights reserved.');
-define('pages', [
-    ['name' => 'HOME', 'action' => 'index'],
-    ['name' => 'CONTACT FORM', 'action' => 'form']
-]);
-?>
